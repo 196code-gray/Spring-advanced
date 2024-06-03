120a42bfbf05a15bd0c40f16da43f8cd3df16455
196code-gray <doco827@gmail.com>
2024-06-03 12:18:40 +0900

    Feat : " 추가 로그 기록" 클라이언트의 요청이 들어오면 진행상황을 로그로 기록 TraceId에서 로그의 개별Id와 깊이를 가지고, TraceStatus에서 개별Id와 시작시간, 메서드명을 가진다.

--- src/main/java/com/spring/advanced/trace/TraceId.java
+++ src/main/java/com/spring/advanced/trace/TraceId.java
@@ -1,2 +1,45 @@
-package com.spring.advanced.trace;public class TraceId {
+package com.spring.advanced.trace;
+
+import java.util.UUID;
+// 요청 Id, level을 가지는 class
+public class TraceId {
+
+    private String id;
+    private int level;
+
+    public TraceId() {
+        this.id = createId();
+        this.level = 0;
+    }
+
+    private TraceId(String id, int i) {
+        this.id = id;
+        this.level = i;
+    }
+
+    private String createId() {
+        return UUID.randomUUID().toString().substring(0, 8); // 앞부분만 사용
+    }
+
+    // 다음 로그 레벨을 위한 메서드
+    public TraceId createNextId(){
+        return new TraceId(id, level + 1);
+    }
+
+    public TraceId createPreviousId(){
+        return new TraceId(id, level -1);
+    }
+
+    // 현재 레벨이 첫번째 레벨인지 체크
+    public boolean isFirstLevel(){
+        return level == 0;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
 }
