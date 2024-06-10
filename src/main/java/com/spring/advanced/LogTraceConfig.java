efbe55e88d9d3b824ba0046308ac908f6b978746
196code-gray <doco827@gmail.com>
2024-06-10 12:39:05 +0900

    Refactor:" 수정 LogConfig"
    동기화를 해결하기 위한 ThreadLocal을 Bean으로 등록.

--- src/main/java/com/spring/advanced/LogTraceConfig.java
+++ src/main/java/com/spring/advanced/LogTraceConfig.java
@@ -1,7 +1,7 @@
 package com.spring.advanced;
 
-import com.spring.advanced.logtrace.FieldLogTrace;
 import com.spring.advanced.logtrace.LogTrace;
+import com.spring.advanced.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +10,6 @@ public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
