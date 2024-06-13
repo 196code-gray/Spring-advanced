d93d936ee530546ea91417c6e880e35bd140ea06
196code-gray <doco827@gmail.com>
2024-06-13 11:43:21 +0900

    Feat:" 추가 Decorator pattern 적용 전"

---
+++ src/test/java/com/spring/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -0,0 +1,16 @@
+package com.spring.advanced.proxy.pureproxy.decorator;
+
+import com.spring.advanced.proxy.pureproxy.decorator.code.Component;
+import com.spring.advanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import com.spring.advanced.proxy.pureproxy.decorator.code.RealComponent;
+import org.junit.jupiter.api.Test;
+
+public class DecoratorPatternTest {
+
+    @Test
+    void noDecorator(){
+        Component realComponent = new RealComponent();
+        DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+}
---
+++ src/test/java/com/spring/advanced/proxy/pureproxy/decorator/code/Component.java
@@ -0,0 +1,5 @@
+package com.spring.advanced.proxy.pureproxy.decorator.code;
+
+public interface Component {
+    String operation();
+}
