a0b39c977d959a21cc0e1c75dc3bd939bc13d838
196code-gray <doco827@gmail.com>
2024-06-13 11:45:28 +0900

    Refactor:" 수정 Decorator pattern 적용 테스트"

--- src/test/java/com/spring/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
+++ src/test/java/com/spring/advanced/proxy/pureproxy/decorator/DecoratorPatternTest.java
@@ -2,6 +2,7 @@ package com.spring.advanced.proxy.pureproxy.decorator;
 
 import com.spring.advanced.proxy.pureproxy.decorator.code.Component;
 import com.spring.advanced.proxy.pureproxy.decorator.code.DecoratorPatternClient;
+import com.spring.advanced.proxy.pureproxy.decorator.code.MessageDecorator;
 import com.spring.advanced.proxy.pureproxy.decorator.code.RealComponent;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,15 @@ public class DecoratorPatternTest {
     void noDecorator(){
         Component realComponent = new RealComponent();
         DecoratorPatternClient client = new DecoratorPatternClient(realComponent);
+        client.execute();
+    }
+
+    @Test
+    void decorator1(){
+        Component realComponent = new RealComponent();
+        Component messageDecorator = new MessageDecorator(realComponent);
+        DecoratorPatternClient client = new DecoratorPatternClient(messageDecorator);
+
         client.execute();
     }
 }
