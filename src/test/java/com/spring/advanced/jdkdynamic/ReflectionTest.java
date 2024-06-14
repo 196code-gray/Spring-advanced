7afc87801ae3c74d752d3b3f967ea5178cc93541
196code-gray <doco827@gmail.com>
2024-06-14 12:17:52 +0900

    Feat:" 추가 what is reflection"
    reflection이란 무엇인지, 어떻게 동적으로 할당 되는지 테스트.

---
+++ src/test/java/com/spring/advanced/jdkdynamic/ReflectionTest.java
@@ -0,0 +1,72 @@
+package com.spring.advanced.jdkdynamic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ReflectionTest {
+/*
+리플랙션이란 무엇인지, 어떻게 사용하는지 알아보자.
+ */
+    @Test
+    void reflection0(){
+        Hello target = new Hello();
+
+        // 공통 로직
+        log.info("start");
+        String result1 = target.callA();
+        log.info("result1 = {}", result1);
+
+        log.info("start");
+        String result2 = target.callB();
+        log.info("result1 = {}", result2);
+    }
+
+    @Test
+    void reflection1() throws Exception {
+        Class classHello = Class.forName("com.spring.advanced.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+
+        Method methodCallA = classHello.getMethod("callA");
+        Object result1 = methodCallA.invoke(target);
+        log.info("result1 = {}", result1);
+
+        Method methodCallB = classHello.getMethod("callB");
+        Object result2 = methodCallB.invoke(target);
+        log.info("result1 = {}", result2);
+
+    }
+
+    @Test
+    void reflection2() throws Exception {
+        Class classHello = Class.forName("com.spring.advanced.jdkdynamic.ReflectionTest$Hello");
+
+        Hello target = new Hello();
+
+        Method methodCallA = classHello.getMethod("callA");
+        dynamicCall(methodCallA, target);
+
+        Method methodCallB = classHello.getMethod("callB");
+        dynamicCall(methodCallB, target);
+    }
+
+    private void dynamicCall(Method method, Object target) throws Exception{
+        log.info("start");
+        Object result = method.invoke(target);
+        log.info("result = {}", result);
+    }
+
+    static class Hello {
+        public String callA(){
+            log.info("callA");
+            return "A";
+        }
+        public String callB(){
+            log.info("callB");
+            return "B";
+        }
+    }
+}
